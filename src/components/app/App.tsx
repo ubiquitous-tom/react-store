c33203d43cb1cd8e5085e12ad7968d7dee93aa9c
Tom Soisoonthorn <omnipresent_tom@hotmail.com>
2019-10-16 10:45:40 -0700

    remove prettier --write from lint-staged

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -13,7 +13,12 @@ const App: React.FC = () => {
           and save to reload.
         </p>
 
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
