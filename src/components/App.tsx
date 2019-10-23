14372e7f338562da833c0c51be97b73e2d155b92
Tom koston <omnipresent_tom@hotmail.com>
2019-10-23 07:30:57 -0700

    add react-cookie and universal-cookie and add sign in layer in App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,6 +7,8 @@ import {
   Link,
   RouteComponentProps,
 } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import Cookies from 'universal-cookie';
 
 // import logo from './logo.svg';
 
@@ -19,9 +21,77 @@ import { NoMatch } from './no-match/NoMatch';
 import { EditBilling } from './edit-billing/EditBilling';
 import { ReviewPurchase } from './reviewPurchase/ReviewPurchase';
 
-// import { TextField } from '../TextField/TextField';
 
 const App: React.FC = () => {
+  const cookies = new Cookies();
+  const [userInfo, setUserInfo] = useState(false);
+
+  const [userCookie, setUserCookie] = useCookies(['ATVSessionCookie']);
+
+  function signIn() {
+    const atvSessionCookie = cookies.get('ATVSessionCookie');
+    const username = 'pat.crews+trial1@valtira.net';
+    const password = 'badPassword';
+    const refererURL = 'localhost';
+    console.log(atvSessionCookie);
+    if (atvSessionCookie) {
+      const signUpSplitScenario = 'TRIAL_SIGNUP';
+      const requestBody = {
+        App: {
+          AppVersion: 'Sign-Up-Website',
+        },
+        Credentials: {
+          Username: username,
+          Password: password,
+        },
+        Request: {
+          SplitScenario: signUpSplitScenario,
+          OperationalScenario: 'SIGNIN',
+          Referer: refererURL,
+        },
+      };
+      console.log(requestBody);
+      fetch('https://dev-api.rlje.net/acorn/initializeapp', {
+        method: 'post',
+        body: JSON.stringify(requestBody),
+      })
+        .then((resp) => {
+          const data = resp.json();
+          console.log(data);
+          return data;
+        })
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  }
+
+  function checkUserLogInStatus() {
+    const atvSessionCookie = cookies.get('ATVSessionCookie');
+    console.log(atvSessionCookie);
+    if (atvSessionCookie) {
+      fetch(`https://dev-api.rlje.net/acorn/profile?SessionID=${atvSessionCookie}`)
+        .then((resp) => {
+          const data = resp.json();
+          console.log(data);
+          return data;
+        })
+        .then((data) => {
+          console.log(data);
+          setUserInfo(data);
+        });
+    } else {
+      setUserInfo(false);
+    }
+  }
+
+  useEffect(() => {
+    if (userInfo) {
+      console.log(userInfo);
+    } else {
+      checkUserLogInStatus();
+    }
+  });
   // const defaultValue = '';
   // const [inputValue, setValue] = useState<string | number>(defaultValue);
 
--- src/components/shared/footer/language-dropdown/LanguageDropdown.tsx
+++ src/components/shared/footer/language-dropdown/LanguageDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ReactNode } from 'react';
+import React, { useState, useEffect, ReactNode } from 'react';
 
 import { GetLanguagesService } from '../../../../services/GetLanguagesService';
 
@@ -11,35 +11,19 @@ export const LanguageDropdown: React.FC<LanguageDropdownProps> = ({ locale }) =>
   const languages = GetLanguagesService();
   const [newLocale, setNewLocale] = useState(locale);
 
+  useEffect(() => {
+    console.log('GetLanguageService useEffect');
+  });
+
   const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
     setNewLocale(event.target.value);
-  };
-
-  const getLanguagesOptions = () => {
-    console.log(languages);
-    // Object.keys(languages).map((key, index, arr) => {
-    //   console.log(index, key, arr, arr[index], languages;
-    //   return <option value="{key}">{index}</option>;
-    // });
-    console.log(typeof languages);
-    // languages.forEach((value, index, arr) => {
-    //   console.log(index, value, arr);
-    // });
-
-    Object.keys(languages).forEach((abbr: string) => {
-      if (hasKey(languages, abbr)) {
-        console.log(abbr); // key
-        console.log(languages[abbr]); // value
-        return <option value={abbr}>{languages[abbr]}</option>;
-      }
-    });
   };
 
   // https://dev.to/kingdaro/indexing-objects-in-typescript-1cgi
   // `keyof any` is short for "string | number | symbol"
   // since an object key can be any of those types, our key can too
   // in TS 3.0+, putting just "string" raises an error
-  function hasKey<O>(obj: O, key: keyof any): key is keyof O {
+  function hasKey<O>(obj: O, key: string | number | symbol): key is keyof O {
     return key in obj;
   }
 
@@ -55,8 +39,14 @@ export const LanguageDropdown: React.FC<LanguageDropdownProps> = ({ locale }) =>
           defaultValue={locale}
           onChange={handleLanguageChange}
         >
-          <option value="en">english</option>
-          <option value="es">spanish</option>
+          {Object.keys(languages).map((abbr, index): ReactNode => {
+            if (hasKey(languages, abbr)) {
+              // Need hasKey() function so Typescript won't throw type error
+              console.log(abbr, index, languages[abbr]);
+              return <option key={index} value={abbr}>{languages[abbr]}</option>;
+            }
+            return null;
+          })}
         </select>
       </label>
     </div>
